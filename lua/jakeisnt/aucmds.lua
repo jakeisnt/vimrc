d1b38544c76ab53bcbab74bbde23c696c8c624df
Jacob Chvatal <jake@isnt.online>
2024-10-08 00:12:49 +0200

    add aucmd to forward file edit history to terminal history

--- lua/jakeisnt/aucmds.lua
+++ lua/jakeisnt/aucmds.lua
@@ -1,4 +1,6 @@
 -- autocommands
+local authgroup = vim.api.nvim_create_augroup("AuthGroup", { clear = true })
+
 vim.api.nvim_create_autocmd("FileType", {
   group = authgroup,
   pattern = "man",
@@ -45,4 +47,23 @@ vim.api.nvim_create_autocmd("FileType", {
   group = authgroup,
   pattern = "*",
   command = "setlocal formatoptions-=cro",
+})
+
+
+-- When a file buffer opens, record it in nushell history
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  group = authgroup,
+  pattern = "*",
+  callback = function()
+    local buftype = vim.bo.buftype
+    local file_path = vim.fn.expand("%:p")
+
+    if buftype == "" and file_path ~= "" then
+      local nushell_history_path = vim.fn.expand("~/.config/nushell/history.txt")
+      local relative_path = vim.fn.fnamemodify(file_path, ":~")
+
+      local nushell_command = string.format("'vi %s\n' | save --append %s", relative_path, nushell_history_path)
+      vim.fn.system(nushell_command)
+    end
+  end
 })
